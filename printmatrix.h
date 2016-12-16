c7373ae79760548018d067ea733081495e9c32a5
Andrew Conant <aconant1@swarthmore.edu>
2016-12-16 15:42:57 -0500

    delete matrix files

--- printmatrix.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <QMatrix4x4>
-#include <QVector3D>
-#include <ostream>
-
-// In case you want to cout a QMatrix4x4
-std::ostream& operator<<(std::ostream& str, const QMatrix4x4& mat);
